1efe918b135d15dccb9b06d33db73df88aec6b7e
DFANNN <737812379@qq.com>
2025-01-20 18:09:30 +0800

    feat(renderer): 搭建基础框架
    - 添加路由配置和布局组件
    - 设置全局样式
    - 更新 App 组件结构
    -引入 vue-router 依赖

---
+++ src/renderer/src/layouts/aside.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="aside-container">
+    <div class="logo-box">
+      <div class="logo">
+        <img src="../assets/logo.png" alt="logo">
+      </div>
+      <div class="logo-title">DFAN-Rename</div>
+    </div>
+    <div class="menu-box">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        :collapse="false"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="2">
+          <el-icon>
+            <icon-menu/>
+          </el-icon>
+          <template #title>Navigator Two</template>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon>
+            <setting/>
+          </el-icon>
+          <template #title>Navigator Four</template>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <el-icon>
+            <setting/>
+          </el-icon>
+          <template #title>这是什么模式了</template>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <el-icon>
+            <setting/>
+          </el-icon>
+          <template #title>那这个了</template>
+        </el-menu-item>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<style scoped lang="less">
+.aside-container {
+  .logo-box {
+    display: flex;
+    align-items: center;
+    height: 85px;
+
+    .logo {
+      width: 48px;
+      height: 48px;
+
+      img {
+        width: 100%;
+      }
+    }
+
+    .logo-title {
+      margin-left: 1rem;
+      font-size: 20px;
+    }
+  }
+
+  .menu-box {
+    height: calc(100vh - 85px);
+    background-color: hotpink;
+  }
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
--- src/renderer/src/layouts/index.vue
+++ src/renderer/src/layouts/index.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    layouts
+    <el-container class="common-layout">
+      <el-aside class="aside">
+        <AsideComponent/>
+      </el-aside>
+      <el-container class="common-container">
+        <el-header class="header">Header</el-header>
+        <el-main class="main">Main</el-main>
+        <el-footer class="footer">Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import AsideComponent from '@renderer/layouts/aside.vue'
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.common-layout {
+  height: 100vh;
+
+  .aside {
+    background-color: #d3dce6;
+    width: 256px;
+    padding: 0 1.5rem;
+    height: 100%;
+  }
+
+  .common-container {
+    .header {
+      background-color: hotpink;
+    }
+
+    .main {
+      background-color: blueviolet;
+    }
+
+    .footer {
+      background-color: brown;
+    }
+  }
+}
+
 
 </style>
