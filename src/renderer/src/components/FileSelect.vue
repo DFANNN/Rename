4a8696a1358b248d74c451b2117e86e09ee4485e
DFANNN <737812379@qq.com>
2025-02-07 23:10:21 +0800

    feat(FileSelect): 优化文件选择对话框样式
    - 添加对话框头部和底部样式
    - 新增当前路径和磁盘路径展示区域
    - 调整输入框和按钮布局
    - 优化样式，增加边框和颜色

--- src/renderer/src/components/FileSelect.vue
+++ src/renderer/src/components/FileSelect.vue
@@ -2,7 +2,7 @@
   <div class="file-select-container">
     <div class="title">文件选择</div>
     <div class="file-select-box">
-      <el-input v-model="input" placeholder="请选择文件夹路径" />
+      <el-input v-model="input" placeholder="请选择文件夹路径"/>
       <CommonButton class="button" @click="dialogVisible = true">浏览文件夹</CommonButton>
     </div>
 
@@ -12,12 +12,27 @@
       :draggable="true"
     >
       <template #header>
-        <div>选择文件路径</div>
-        <el-icon>
-          <Close />
-        </el-icon>
+        <div class="dialog-header">
+          <div>选择文件路径</div>
+          <el-icon>
+            <Close/>
+          </el-icon>
+        </div>
+
       </template>
-      <div>This is a message</div>
+      <div class="dialog-body">
+        <div class="current-path-box">
+          <div>当前路径：</div>
+          <div>本地磁盘c</div>
+          <div>Users</div>
+          <div>Document</div>
+        </div>
+        <div class="disk-path-box">
+          <div>本地磁盘c</div>
+          <div>本地磁盘d</div>
+          <div>本地磁盘e</div>
+        </div>
+      </div>
       <template #footer>
         <div class="dialog-footer">
           <CommonButton @click="dialogVisible = false">取消</CommonButton>
@@ -30,7 +45,7 @@
 
 <script setup lang="ts">
 import CommonButton from "@renderer/components/CommonButton.vue";
-import { Close } from "@element-plus/icons-vue";
+import {Close} from "@element-plus/icons-vue";
 
 const input = ref("");
 const dialogVisible = ref(false);
@@ -54,5 +69,41 @@ const dialogVisible = ref(false);
       margin-top: 1rem;
     }
   }
+
+
+  .dialog-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 1.5rem;
+    border-bottom: 1px solid var(--border-color);
+    font-size: 16px;
+    color: var(--text-color);
+  }
+
+  .dialog-body {
+    padding: 0 1.5rem 1rem;
+
+    .current-path-box {
+      display: flex;
+      color: var(--text-color);
+      font-size: 14px;
+      margin-bottom: 1rem;
+
+    }
+
+    .disk-path-box {
+      padding: 1rem 1.5rem;
+      border-radius: 0.5rem;
+      color: var(--text-color);
+      border: 1px solid var(--border-color);
+    }
+  }
+
+}
+
+:deep(.el-dialog) {
+  padding: 0;
+  background-color: var(--background-color);
 }
 </style>
