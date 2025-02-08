b5ed0f6b89225fe81d6f9c0b1528a65d70536597
DFANNN <737812379@qq.com>
2025-02-08 11:19:48 +0800

    feat(components): 新增磁盘和文件夹图标并优化文件选择组件
    - 新增 DiskIcon 和 FolderIcon 组件
    - 重构 FileSelect 组件，添加磁盘列表和当前路径显示
    - 优化 FileSelect 组件样式，增加面包屑导航

--- src/renderer/src/components/FileSelect.vue
+++ src/renderer/src/components/FileSelect.vue
@@ -2,7 +2,7 @@
   <div class="file-select-container">
     <div class="title">文件选择</div>
     <div class="file-select-box">
-      <el-input v-model="input" placeholder="请选择文件夹路径"/>
+      <el-input v-model="input" placeholder="请选择文件夹路径" />
       <CommonButton class="button" @click="dialogVisible = true">浏览文件夹</CommonButton>
     </div>
 
@@ -15,22 +15,44 @@
         <div class="dialog-header">
           <div>选择文件路径</div>
           <el-icon>
-            <Close/>
+            <Close />
           </el-icon>
         </div>
 
       </template>
       <div class="dialog-body">
         <div class="current-path-box">
-          <div>当前路径：</div>
-          <div>本地磁盘c</div>
-          <div>Users</div>
-          <div>Document</div>
+          <div class="current-title">
+            <el-icon class="current-title-icon">
+              <FolderIcon />
+            </el-icon>
+            <div>
+              当前路径：
+            </div>
+          </div>
+          <div class="current-path">
+            <div>
+              <span class="path-name">本地磁盘c</span>
+              <span class="separator">/</span>
+            </div>
+            <div>
+              <span class="path-name">Users</span>
+              <span class="separator">/</span>
+            </div>
+            <div>
+              <span class="path-name">Document</span>
+              <span class="separator">/</span>
+            </div>
+          </div>
+
         </div>
         <div class="disk-path-box">
-          <div>本地磁盘c</div>
-          <div>本地磁盘d</div>
-          <div>本地磁盘e</div>
+          <div class="disk-box" v-for="disk in diskList">
+            <el-icon class="disk-icon">
+              <DiskIcon />
+            </el-icon>
+            <div>{{ disk }}</div>
+          </div>
         </div>
       </div>
       <template #footer>
@@ -45,10 +67,14 @@
 
 <script setup lang="ts">
 import CommonButton from "@renderer/components/CommonButton.vue";
-import {Close} from "@element-plus/icons-vue";
+import FolderIcon from "@renderer/components/icon/FolderIcon.vue";
+import DiskIcon from "@renderer/components/icon/DiskIcon.vue";
+import { Close, Folder } from "@element-plus/icons-vue";
 
 const input = ref("");
 const dialogVisible = ref(false);
+
+const diskList = ref(["本地磁盘C", "本地磁盘D", "本地磁盘E", "本地磁盘F", "本地磁盘G"]);
 </script>
 
 <style scoped lang="less">
@@ -90,16 +116,66 @@ const dialogVisible = ref(false);
       font-size: 14px;
       margin-bottom: 1rem;
 
+      .current-title {
+        display: flex;
+        align-items: center;
+
+        .current-title-icon {
+          font-size: 18px;
+          margin-right: 0.25rem;
+          //color: #EAB308;
+          color: var(--theme-common-color);
+        }
+      }
+
+      .current-path {
+        display: flex;
+        align-items: center;
+
+        .path-name {
+          cursor: pointer;
+
+          &:hover {
+            color: var(--theme-common-color);
+            text-decoration: underline;
+          }
+        }
+
+        .separator {
+          margin: 0 0.5rem;
+        }
+      }
     }
 
     .disk-path-box {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
       padding: 1rem 1.5rem;
       border-radius: 0.5rem;
       color: var(--text-color);
       border: 1px solid var(--border-color);
+      height: 300px;
+      overflow-y: auto;
+
+      .disk-box {
+        display: flex;
+        align-items: center;
+
+        .disk-icon {
+          margin-right: 0.5rem;
+          font-size: 22px;
+          color: var(--theme-common-color);
+        }
+      }
+
+
     }
   }
 
+  .dialog-footer {
+    padding: 0.5rem 1.5rem;
+    border-top: 1px solid var(--border-color);
+  }
 }
 
 :deep(.el-dialog) {
