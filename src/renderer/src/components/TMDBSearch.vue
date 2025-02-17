74c6751a511301e0fc8db386683dcb8ed84b5c5c
DFANNN <737812379@qq.com>
2025-02-17 14:54:54 +0800

    feat(TMDBSearch): 增加分页功能和无数据提示
    - 添加分页组件，实现数据分页显示
    -增加无数据提示，优化用户体验
    - 调整搜索接口，支持分页查询
    -优化组件样式，统一代码格式

--- src/renderer/src/components/TMDBSearch.vue
+++ src/renderer/src/components/TMDBSearch.vue
@@ -11,12 +11,13 @@
         <div class="dialog-header">
           <div>TMDB搜索</div>
           <el-icon class="close-icon" @click="cancel">
-            <Close/>
+            <Close />
           </el-icon>
         </div>
       </template>
       <template #default>
         <div class="dialog-body">
+          <!--TODO:1.未做加载loading效果；2.做懒加载；3.用element plus滚动条组件 -->
           <div class="body-header-box">
             <el-input
               v-model="name"
@@ -27,7 +28,7 @@
             </el-input>
             <CommonButton class="search-btn" @click="search">搜索</CommonButton>
           </div>
-          <div class="body-container-box">
+          <div class="body-container-box" v-show="TMDBResult.length">
             <div v-for="TVSeries in TMDBResult" class="body-container-item" @click="selectTVSeriesId = TVSeries.id">
               <img :src="`https://image.tmdb.org/t/p/original${TVSeries.poster_path}`" alt="poster"
                    :class="{'is-active':selectTVSeriesId === TVSeries.id}">
@@ -42,10 +43,19 @@
               <div>{{ TVSeries.first_air_date }}</div>
             </div>
           </div>
-
+          <!-- TODO:未改主题色         -->
+          <el-pagination
+            v-model:current-page="currentPage"
+            :total="total"
+            :page-size="20"
+            hide-on-single-page
+            layout="prev, pager, next"
+            @current-change="search()"
+            class="pagination"
+          />
+          <div class="body-no-data" v-show="!TMDBResult.length">暂无数据</div>
         </div>
       </template>
-
       <template #footer>
         <div class="dialog-footer">
           <CommonButton @click="cancel">取消</CommonButton>
@@ -58,19 +68,19 @@
 
 <script setup lang="ts">
 import CommonButton from "@renderer/components/CommonButton.vue";
-import {Close} from "@element-plus/icons-vue";
-import {ElMessage} from "element-plus";
+import { Close } from "@element-plus/icons-vue";
+import { ElMessage } from "element-plus";
 
 interface ITMDBResultItem {
   // 封面图地址
-  poster_path: string
+  poster_path: string;
   // 名称
-  name: string
+  name: string;
   // 年份
-  first_air_date: string
+  first_air_date: string;
   // 描述
-  overview: string
-  id: number
+  overview: string;
+  id: number;
 }
 
 const publicStore = usePublicStore();
@@ -83,9 +93,13 @@ const dialogVisible = ref(false);
 const TMDBResult = ref<ITMDBResultItem[]>([]);
 
 // input输入的名称
-const name = ref('');
+const name = ref("");
 // 当前选择的电视剧
-const selectTVSeriesId = ref()
+const selectTVSeriesId = ref();
+// 当前页码
+const currentPage = ref(1);
+// 数据总条数
+const total = ref(0);
 
 
 const cancel = () => {
@@ -95,44 +109,44 @@ const cancel = () => {
 // 确定
 const confirm = () => {
   if (!selectTVSeriesId.value) {
-    ElMessage('请选择电视剧')
-    return
+    ElMessage("请选择一个电视剧");
+    return;
   }
-  diskStore.TVSeriesModeForm.name = TMDBResult.value.filter((item) => item.id === selectTVSeriesId.value)[0].name
+  diskStore.TVSeriesModeForm.name = TMDBResult.value.filter((item) => item.id === selectTVSeriesId.value)[0].name;
   dialogVisible.value = false;
 };
 
 const showDialog = () => {
   dialogVisible.value = true;
-}
+};
 
 // 搜索
 const search = async () => {
   // url地址
-  const url = `https://api.themoviedb.org/3/search/tv?query=${name.value}&language=zh-CN&page=1`
+  const url = `https://api.themoviedb.org/3/search/tv?query=${name.value}&language=zh-CN&page=${currentPage.value}`;
   // api 配置项
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWNkMDQ1NGM5ODMxOTA1ZDFiMDk1ZDNlZDg3NWQ0NCIsIm5iZiI6MTczOTQ0MDM0NS42NDQsInN1YiI6IjY3YWRjMGQ5NTMzNTNmOWJiYTM2ZWVmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AqO9jtKsKdb0w3sSmkBlkyztT1U4l1VmmntRXfpbGfI'
+      accept: "application/json",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWNkMDQ1NGM5ODMxOTA1ZDFiMDk1ZDNlZDg3NWQ0NCIsIm5iZiI6MTczOTQ0MDM0NS42NDQsInN1YiI6IjY3YWRjMGQ5NTMzNTNmOWJiYTM2ZWVmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AqO9jtKsKdb0w3sSmkBlkyztT1U4l1VmmntRXfpbGfI"
     }
   };
-  console.log(url)
   try {
     const response = await fetch(url, options);
-    console.log(response)
+    console.log(response);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
     TMDBResult.value = data.results;
+    total.value = data.total_results;
     console.log(data);
   } catch (error) {
-    console.error('Fetch error:', error);
+    console.error("Fetch error:", error);
   }
 
-}
+};
 </script>
 
 <style scoped lang="less">
@@ -163,7 +177,7 @@ const search = async () => {
   }
 
   .dialog-body {
-    padding: 0 1.5rem 1rem;
+    padding: 0 1.5rem;
 
     .body-header-box {
       margin-bottom: 1rem;
@@ -211,6 +225,24 @@ const search = async () => {
           cursor: pointer;
         }
       }
+    }
+
+    .pagination {
+      display: flex;
+      align-items: center;
+      justify-content: end;
+      margin-top: 1rem;
+    }
+
+    .body-no-data {
+      height: 50vh;
+      display: grid;
+      grid-template-columns:  1fr;
+      align-items: center;
+      text-align: center;
+      padding: 1rem 1.5rem;
+      border-radius: 0.5rem;
+      border: 1px solid var(--border-color);
     }
   }
 
